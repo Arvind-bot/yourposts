c52e428e352adac24fabcaea149b6918115ad35f
Arvind Kumar <arvindof7@gmail.com>
2020-11-11 00:08:49 +0530

    Changed title, added favicon and made some styling adjustments

--- src/components/post-image/post-image.component.jsx
+++ src/components/post-image/post-image.component.jsx
@@ -1,4 +1,4 @@
 import React from "react";
 import "./post-image.styles.scss";
 
-export const PostImage = ({ imageUrl }) => <div className="post-image-box-style"><img  className="post-image-style" src={imageUrl} alt="post image" /></div>;
+export const PostImage = ({ imageUrl }) => <div className="figure-container"><img  className="" src={imageUrl} alt="post" /></div>;
